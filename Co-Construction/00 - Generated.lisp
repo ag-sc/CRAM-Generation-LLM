a18a06aec2ed19c9919c41fc441858e87e709e8f
Janfiderheld <Janfitoe@web.de>
2023-07-25 15:28:21 +0200

    add co-construction results

---
+++ Co-Construction/00 - Generated.lisp
@@ -0,0 +1,39 @@
+(<- (desig:action-grounding ?action-designator (pick-up-object ?arm
+                                                            ?gripper-opening
+                                                            ?distance
+                                                            ?left-reach-poses
+                                                            ?right-reach-poses
+                                                            ?left-grasp-poses
+                                                            ?right-grasp-poses
+                                                            (?left-lift-pose)
+                                                            (?right-lift-pose)
+                                                            (?left-2nd-lift-pose)
+                                                            (?right-2nd-lift-pose)
+                                                            ?object-designator
+                                                            ?environment-obj))
+    (spec:property ?action-designator (:type :picking-up))
+    (spec:property ?action-designator (:object ?object-designator))
+    (spec:property ?object-designator (:type ?object-type))
+    (-> (spec:property ?action-designator (:arm ?arm))
+        (true)
+        (and (cram-robot-interfaces:robot ?robot)
+             (cram-robot-interfaces:arm ?robot ?arm)))
+    (spec:property ?action-designator (:distance ?distance))
+    (lisp-fun obj-int:get-object-type-gripper-opening ?object-type ?gripper-opening)
+    (lisp-fun get-object-pose-and-transform ?object-designator ?btr-environment
+              (?object-pose ?object-transform))
+    (lisp-fun obj-int:get-object-grasping-poses ?object-designator
+              :object-prismatic :left :pick-up ?object-transform ?left-poses)
+    (lisp-fun obj-int:get-object-grasping-poses ?object-designator
+              :object-prismatic :right :pick-up ?object-transform ?right-poses)
+    (lisp-fun cram-mobile-pick-place-plans::extract-pick-up-manipulation-poses
+              ?arm ?left-poses ?right-poses
+              (?left-reach-poses ?right-reach-poses
+                                 ?left-grasp-poses ?right-grasp-poses
+                                 ?left-lift-poses ?right-lift-poses))
+    (-> (lisp-pred identity ?left-lift-poses)
+        (equal ?left-lift-poses (?left-lift-pose ?left-2nd-lift-pose))
+        (equal (NIL NIL) (?left-lift-pose ?left-2nd-lift-pose)))
+    (-> (lisp-pred identity ?right-lift-poses)
+        (equal ?right-lift-poses (?right-lift-pose ?right-2nd-lift-pose))
+        (equal (NIL NIL) (?right-lift-pose ?right-2nd-lift-pose))))
---
+++ Co-Construction/07 - Remove container setting.lisp
@@ -0,0 +1,45 @@
+(<- (desig:action-grounding ?action-designator (pick-up-object ?arm
+                                                            ?gripper-opening
+                                                            ?distance
+                                                            ?left-reach-poses
+                                                            ?right-reach-poses
+                                                            ?left-grasp-poses
+                                                            ?right-grasp-poses
+                                                            (?left-lift-pose)
+                                                            (?right-lift-pose)
+                                                            (?left-2nd-lift-pose)
+                                                            (?right-2nd-lift-pose)
+                                                            ?joint-name
+                                                            ?environment-obj))
+    (spec:property ?action-designator (:type :picking-up))
+    (spec:property ?action-designator (:object ?container-designator))
+    (-> (spec:property ?action-designator (:arm ?arm))
+        (true)
+        (and (cram-robot-interfaces:robot ?robot)
+             (cram-robot-interfaces:arm ?robot ?arm)))
+    (spec:property ?action-designator (:distance ?distance))
+    (spec:property ?container-designator (:type ?container-type))
+    (obj-int:object-type-subtype :container ?container-type)
+    (lisp-fun get-container-link ?container-name ?btr-environment ?container-link)
+    (lisp-fun get-connecting-joint ?container-link ?connecting-joint)
+    (lisp-fun cl-urdf:name ?connecting-joint ?joint-name)
+    (btr:bullet-world ?world)
+    (lisp-fun btr:object ?world ?btr-environment ?environment-obj)
+    (lisp-fun obj-int:get-object-type-gripper-opening ?container-type ?gripper-opening)
+    (lisp-fun get-container-pose-and-transform ?container-name ?btr-environment
+              (?container-pose ?container-transform))
+    (lisp-fun obj-int:get-object-grasping-poses ?container-name
+              :container-prismatic :left :pick-up ?container-transform ?left-poses)
+    (lisp-fun obj-int:get-object-grasping-poses ?container-name
+              :container-prismatic :right :pick-up ?container-transform ?right-poses)
+    (lisp-fun cram-mobile-pick-place-plans::extract-pick-up-manipulation-poses
+              ?arm ?left-poses ?right-poses
+              (?left-reach-poses ?right-reach-poses
+                                 ?left-grasp-poses ?right-grasp-poses
+                                 ?left-lift-poses ?right-lift-poses))
+    (-> (lisp-pred identity ?left-lift-poses)
+        (equal ?left-lift-poses (?left-lift-pose ?left-2nd-lift-pose))
+        (equal (NIL NIL) (?left-lift-pose ?left-2nd-lift-pose)))
+    (-> (lisp-pred identity ?right-lift-poses)
+        (equal ?right-lift-poses (?right-lift-pose ?right-2nd-lift-pose))
+        (equal (NIL NIL) (?right-lift-pose ?right-2nd-lift-pose))))
