edd745719f92ded7cf263e751a0f7141ef14183d
Janfiderheld <Janfitoe@web.de>
2023-08-18 16:41:34 +0200

    calculate spearman instead of pearson correlation

--- eval/correlation.py
+++ eval/correlation.py
@@ -1,7 +1,7 @@
-from scipy.stats.stats import pearsonr
+from scipy.stats import pearsonr, spearmanr
 
 
-def calculate_correlations(model_name: str, fix_col_name: str):
+def calculate_correlations(model_name: str, fix_col_name: str, use_spearman=True):
     from inout import ResultReader
     from model import ResultColumnHeaders
 
@@ -12,13 +12,17 @@ def calculate_correlations(model_name: str, fix_col_name: str):
 
     print(f"Calculating the correlation to the {fix_col_name} metric for the {model_name} model:")
     for m in metrics:
-        corr = evaluate_significance(model_df[fix_col_name], model_df[m])
+        corr = evaluate_significance(model_df[fix_col_name], model_df[m], use_spearman)
         print(f"Correlation to {m} is {corr[1]} (p = {round(corr[2], 5)}) with r = {round(corr[0], 3)}")
     print("\n")
 
 
-def evaluate_significance(col1, col2) -> (float, str, float):
-    res = pearsonr(col1, col2)
+def evaluate_significance(col1, col2, use_spearman: bool) -> (float, str, float):
+    if use_spearman:
+        res = spearmanr(col1, col2)
+    else:
+        res = pearsonr(col1, col2)
+
     if res[1] <= 0.05:
         return res[0], 'significant', res[1]
     else:
