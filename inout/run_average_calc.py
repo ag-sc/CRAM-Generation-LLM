f72fae02e34f2cc0186a16bd1882b6b19c87f47a
Janfiderheld <Janfitoe@web.de>
2023-08-21 15:54:52 +0200

    calculate average over the whole 3 models

--- inout/run_average_calc.py
+++ inout/run_average_calc.py
@@ -6,10 +6,11 @@ from inout import ResultReader, import_actions
 def calculate_averages():
     from model import ModelType
     for mt in ModelType:
-        average_specific_model(mt)
+        average_model_action_specific(mt)
+        average_whole_model(mt)
 
 
-def average_specific_model(model_name: str):
+def average_model_action_specific(model_name: str):
     from model import ResultColumnHeaders
     no_runs = 5
     metrics = [ResultColumnHeaders.wup, ResultColumnHeaders.glove, ResultColumnHeaders.smd, ResultColumnHeaders.bleu, ResultColumnHeaders.r1,
@@ -39,4 +40,24 @@ def average_specific_model(model_name: str):
                 avg_val = val / no_runs
                 avg_df.loc[row_count, m] = avg_val
             row_count += 1
+    avg_df.to_csv(f'./data/results/average action results {model_name}.csv')
+
+
+def average_whole_model(model_name: str):
+    from model import ResultColumnHeaders
+    no_desigs = 5*9*8
+    metrics = [ResultColumnHeaders.bleu, ResultColumnHeaders.r1, ResultColumnHeaders.r2, ResultColumnHeaders.rl, ResultColumnHeaders.cbs,
+               ResultColumnHeaders.chrf]
+    results = ResultReader.read_all_results()
+    avg_df = pd.DataFrame(columns=metrics)
+
+    model_results = results[results[ResultColumnHeaders.model] == model_name]
+    assert len(model_results) == no_desigs
+    for m in metrics:
+        val = 0.0
+        for idx, row in model_results.iterrows():
+            val += row[m]
+        avg_val = val / no_desigs
+        avg_df.loc[0, m] = avg_val
     avg_df.to_csv(f'./data/results/average results {model_name}.csv')
+
